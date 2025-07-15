34aa7cba21e24a5ef853ba8fd5b8c8fcd80f0471
FarhanHaider999 <haiderfarhan178@gmail.com>
2025-07-15 12:37:57 +0500

    Added URL Schema

---
+++ models/Url.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const urlSchema = new mongoose.Schema(
+  {
+    url: {
+      type: String,
+      required: [true, "URL is required"],
+      validate: {
+        validator: function (v) {
+          return validator.isURL(v, {
+            protocols: ["http", "https"],
+            require_protocol: true,
+          });
+        },
+        message: "Please provide a valid URL with http or https protocol",
+      },
+    },
+    shortCode: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+    accessCount: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//Method to increment access count
+urlSchema.methods.incrementAccessCount = function () {
+  this.accessCount += 1;
+  this.save();
+};
